0130c44fde5714cb4f0ab4659e1c6f63c365f320
charliesabino <sabinocharlie@gmail.com>
2023-06-05 11:03:49 -0400

    Add let statement parsing

--- ast/ast.go
+++ ast/ast.go
@@ -41,3 +41,7 @@ type Identifier struct {
 	Token token.Token
 	Value string
 }
+
+func (i *Identifier) expressionNode()      {}
+func (i *Identifier) TokenLiteral() string { return i.Token.Literal }
+func (i *Identifier) String() string       { return i.Value }
