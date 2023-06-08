d72d8ac23a2e8a3a8841169b8783c1a3ba2a82b4
charliesabino <sabinocharlie@gmail.com>
2023-06-07 20:39:18 -0400

    Start REPL

--- repl/repl.go
+++ repl/repl.go
@@ -3,6 +3,7 @@ package repl
 import (
 	"bufio"
 	"fmt"
+	"gorilla/evaluator"
 	"gorilla/lexer"
 	"gorilla/parser"
 	"io"
@@ -29,9 +30,11 @@ func Start(in io.Reader, out io.Writer) {
 			printParserErrors(out, p.Errors())
 			continue
 		}
-
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
