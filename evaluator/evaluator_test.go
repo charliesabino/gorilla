0309a9d2febfb62c4a42c2c853442424e426f5ed
charliesabino <sabinocharlie@gmail.com>
2023-06-08 14:10:13 -0400

    Add let statement tests, extend error handling tests

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -225,6 +225,10 @@ func TestErrorHandling(t *testing.T) {
         return 1; `,
 			expectedMessage: "unknown operator: BOOLEAN + BOOLEAN",
 		},
+		{
+			input:           "foobar",
+			expectedMessage: "identifier not found: foobar",
+		},
 	}
 
 	for _, tt := range tests {
@@ -240,5 +244,21 @@ func TestErrorHandling(t *testing.T) {
 		if errObj.Message != tt.expectedMessage {
 			t.Errorf("wrong error message. expected=%q, got=%q", tt.expectedMessage, errObj.Message)
 		}
+	}
+}
+
+func TestLetStatement(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"let a = 5; a;", 5},
+		{"let a = 5 * 5; a;", 25},
+		{"let a = 5; let b = a; b;", 5},
+		{"let a = 5; let b = a; let c = a + b + 5; c;", 15},
+	}
+
+	for _, tt := range tests {
+		testIntegerObject(t, testEval(tt.input), tt.expected)
 	}
 }
